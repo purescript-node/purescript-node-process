e55ae2163c502405f30f99b38093b061948349b1
Alexander <alex@abushkevi.ch>
2017-08-03 12:56:28 +0000

    Added OpenBSD platform

--- src/Node/Platform.purs
+++ src/Node/Platform.purs
@@ -8,6 +8,7 @@ import Data.Maybe (Maybe(..))
 data Platform
   = Darwin
   | FreeBSD
+  | OpenBSD
   | Linux
   | SunOS
   | Win32
@@ -16,6 +17,7 @@ data Platform
 toString :: Platform -> String
 toString Darwin  = "darwin"
 toString FreeBSD = "freebsd"
+toString OpenBSD = "openbsd"
 toString Linux   = "linux"
 toString SunOS   = "sunos"
 toString Win32   = "win32"
@@ -23,6 +25,7 @@ toString Win32   = "win32"
 fromString :: String -> Maybe Platform
 fromString "darwin"  = Just Darwin
 fromString "freebsd" = Just FreeBSD
+fromString "openbsd" = Just OpenBSD
 fromString "linux"   = Just Linux
 fromString "sunos"   = Just SunOS
 fromString "win32"   = Just Win32
@@ -31,6 +34,7 @@ fromString _         = Nothing
 instance showPlatform :: Show Platform where
   show Darwin  = "Darwin"
   show FreeBSD = "FreeBSD"
+  show OpenBSD = "OpenBSD"
   show Linux   = "Linux"
   show SunOS   = "SunOS"
   show Win32   = "Win32"
